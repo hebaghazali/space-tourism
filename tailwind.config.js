bdc419d4c6ade02dd1dd5ee017cf5b47872e6957
Heba Ghazaly <hebbaghazaly@gmail.com>
2022-08-10 13:58:08 +0200

    Tailwind CSS configuration

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        barlow: ['Barlow', 'sans-serif'],
+        'barlow-condensed': ['Barlow Condensed', 'sans-serif'],
+        bellefair: ['Bellefair', 'serif'],
+      },
+      backgroundImage: {
+        home: "url('../public/assets/home/background-home-desktop.jpg')",
+      },
+      borderWidth: {
+        3: '3px',
+      },
+      colors: {
+        black: '#0B0D17',
+      },
+      boxShadow: {
+        w: '1px 1px 0 88px rgba(255, 255, 255, 0.1)',
+      },
+    },
+  },
+  plugins: [],
+};
