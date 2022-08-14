75d7ebdf81f28adfd64dd360f1167158b3d12074
Heba Ghazaly <hebbaghazaly@gmail.com>
2022-08-14 20:37:51 +0200

    Position fix

--- components/Typography/Heading.js
+++ components/Typography/Heading.js
@@ -1,6 +1,6 @@
 const Heading = ({ children, idx }) => {
   return (
-    <div className='absolute top-[4.75rem] left-[11.56%] tracking-wider flex'>
+    <div className='absolute top-[4.75rem] left-[10.4rem] tracking-wider flex'>
       <p className='opacity-25 pr-4 font-bold text-3xl leading-4'>{idx}</p>
       {children}
     </div>
